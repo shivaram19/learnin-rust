e86a65f7cdf1465c63bc555c46fd5e1707ae3694
ShivaramMorigala <shivaramiitmb085@gmail.com>
2024-09-29 02:34:29 +0530

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,67 @@
-fn main() {
-    println!("Hello, world!");
-    println!("{}",is_even(20));
-    println!("the number 5 in the sequence {}",find_fib(5));
+struct User {
+    single: bool,
+    username:String,
+    interests:String
 }
 
+struct Rect {
+    h:i32,
+    w:i32
+}
 
-fn is_even(num : i32) -> bool {
-    if num%2 == 0 {
-        return true;
-    }
-    return false;
+enum Direction {
+    North,
+    South,
+    East,
+    West
 }
-        
-fn find_fib(num : u32) -> u32 {
-    if num == 1 || num == 2  {
-        return num ;
-    }else {
-        return find_fib(num-1) + find_fib(num-2)
+
+enum Shape{
+    circle(f64),
+    square(f64),
+    rectangle(f64, f64)
+}
+
+fn calculate_area(shape : Shape)-> f64 {
+     match shape{
+        Shape::circle(radius) => 3.14*radius*radius,
+        Shape::square(side_length)  => side_length*side_length,
+        Shape::rectangle(length,breadth ) => length*breadth
     }
 }
+impl Rect {
+    fn area(&self)->i32{
+        return self.h*self.w;
+    }
+
+    fn perimeter(&self)->i32{
+        return 2*(self.h + self.w);
+    }
+}
+
+pub fn main(){
+    let user1 = User{
+        single:false,
+        username:String::from("Darling"),
+        interests:String::from("Badminton")
+    };
+
+    let rect = Rect{
+        h:3,
+        w:7
+    };
+    println!("the area of rectangle is {}",rect.area());
+    println!("the perimeter of rectangle is {}",rect.perimeter());
+    println!("username of userr1 is {:?}",user1.username);
+
+
+    // enums 
+    // let my_direction = Direction::East;
+    // println!("now i'm going in direction {}",my_direction);
+
+    // patternMatching
+    let circle = Shape::circle(5.0);
+    let square = Shape::square(40.0);
+    let rectangle = Shape::rectangle(70.0, 20.0);
+    println!("the area of the circle is {}",calculate_area(circle));
+} 
